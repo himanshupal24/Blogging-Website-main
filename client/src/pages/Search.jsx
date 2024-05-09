c317fe39cbf19e27358fa94c4810748114890e4c
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-09 07:21:49 +0530

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,11 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='development'>Development</option>
+              <option value='design'>Design</option>
+              <option value='sports'>Sports</option>
+              <option value='news'>News</option>
+              <option value='smart-phones'>Smart Phones</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
